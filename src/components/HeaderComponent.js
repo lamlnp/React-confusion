096819545b6034e1f39b97c8a784bc5277831e65
Pham Ngoc Lam <lamnp16@gmail.com>
2022-07-21 07:50:59 +0700

    Exercise 29 - navbar fix needed

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,27 +1,73 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
-
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { Card } from "reactstrap";
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isNavOpen: false
+        };
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+        this.setState({
+            isNavOpen: !this.state.isNavOpen
+        });
+    }
+
     render() {
         return (
             <React.Fragment>
-                <Navbar dark>
+                <Navbar dark expannd='md'>
                     <div className="container">
-                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarBrand className='mr-auto' href="/">
+                            <img src="public/assets/images/logo.png" alt="Ristorante Con Fusion" height="30" width="41" />
+                        </NavbarBrand>
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar>
+                                <NavItem>
+                                    <NavLink className='nav-link' to='/home'>
+                                        <span className='fa fa-home fa-lg'></span> Home
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className='nav-link' to='/about'>
+                                        <span className='fa fa-info fa-lg'></span> About Us
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className='nav-link' to='/menu'>
+                                        <span className='fa fa-list fa-lg'></span> Menu
+                                    </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink className='nav-link' to='/contactus'>
+                                        <span className='fa fa-address-card fa-lg'></span> Contact Us
+                                    </NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
-                <Jumbotron>
+                <Card body
+                    inverse
+                    style={{
+                        backgroundColor: '#9575CD',
+                        borderColor: '#9575CD'
+                    }}>
                     <div className="container">
                         <div className="row row-header">
                             <div className="col-12 col-sm-6">
-                                <div className="jumbotron">
-                                    <h1>Ristorante Con Fusion</h1>
-                                    <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses</p>
-                                </div>
+                                <h1>Ristorante con Fusion</h1>
+                                <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                             </div>
                         </div>
                     </div>
-                </Jumbotron>
+                </Card>
             </React.Fragment>
         )
     }
