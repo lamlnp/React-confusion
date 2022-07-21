7f9e4139404df5c155074915242ea2f40159148d
Pham Ngoc Lam <lamnp16@gmail.com>
2022-07-21 13:49:12 +0700

    Lab 6

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
@@ -12,7 +13,6 @@ import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
-
     constructor(props) {
         super(props);
 
@@ -24,10 +24,13 @@ class Main extends Component {
         };
     }
 
-
+    onDishSelect(dish) {
+        this.setState({
+            selectedDish: dish
+        });
+    }
 
     render() {
-
         const HomePage = () => {
             return (
                 <Home
@@ -36,14 +39,23 @@ class Main extends Component {
                     leader={this.state.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
-        }
+        };
+
+        const DishWithId = ({ match }) => {
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+            );
+        };
 
         return (
-            <div className="App">
+            <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
@@ -52,6 +64,5 @@ class Main extends Component {
         );
     }
 }
-
 
 export default Main;
